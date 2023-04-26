fcf7d1182aff9e43bb465d5a859011a819cc294f
Dmitry <free-doom46@mail.ru>
2023-04-26 22:55:05 +0300

    feat: creat func to add keyCode to obj

--- shelter/script.js
+++ shelter/script.js
@@ -1,78 +1,50 @@
+//1. Входная точка языка
+
+const textArea = document.querySelector('#textarea');
+const word = document.querySelector('.word');
 
 
+const keyCodes = { 
+	'KeyF' : ['f', 'а']
 
-// function dubleArr (mass) {
-// 	let mix = []
-// 	let first = mix.push([...mass.slice(0,3)]);
-// 	let second = mix.push([...mass.slice(3,6)]);
-// 	let point = mass.slice(0,3).sort(() => Math.random() - 0.5)[0]
-// 	let third = mix.push([...mass.slice(6), point])
-// 	mainArr.length = 0;
-//  return mix;
-// }
-
-// console.log(dubleArr(mainArr)) 
-// console.log(mainArr)
-//        ----------0  -------1
-let mainArr = [0,1,2,3,4,5,6,7]
-
-let mainArr48 = []
-
-
-for ( let i = 0; i < 3; i++) {
-	mainArr48.push(...mainArr)
 }
 
-let generateArray = () => {
-	let mainArrShake = []
-	for(let i=0; i<3;i++) {
-		mainArr48.slice(0,6).sort(() => Math.random() - 0.5).push(mainArrShake);
-		console.log(mainArr48)
-	}
-	return mainArrShake;
-}
-console.log(generateArray())
+// console.log(keyCodes.KeyF[0])
+// const codeValue =  Object.values(keyCodes);
+// console.log(codeValue)
+document.addEventListener('keydown',(evt) => {
+	// for(let key in keyCodes) {
+	// 	if(evt.keyCode == key) {
+	// 		console.log(`Нажата ${key}`)
+	// 	}
+	// 	else {
+	// 		console.log('Нихуя не нажато')
+	// 	}
+	// }
 
-// console.log(mainArr48)
+	
+// console.log(typeof evt.code)
+// console.log(typeof evt.code)
+})
 
-
-let MassMass = [];
-let safe = [];
-const SIZE = 8;
-
-
-let arr = mainArr.sort(() => Math.random() - 0.5);
-
-for (let i = 0; i < arr.length; i += SIZE ) {
-	MassMass.push(arr.slice(i, i + SIZE));
-}
-MassMass.splice(2)
-
-let next = (mas) => { 
-	safe = [...mas[1]]
-	MassMass[0] = MassMass[1]
-	MassMass[1] = mainArr.filter(num => !safe.includes(num)).sort(() => Math.random() - 0.5).slice(0,SIZE);
-	return;
+function hendleClikc (evt) {
+	const code = evt.target.innerText;
+	textArea.value += code;
+	// word.removeEventListener();
 }
 
-let prev = (mas) => {
-	MassMass[1] = MassMass[0];
-	MassMass[0] = mainArr.filter(num => !MassMass[1].includes(num)).sort(() => Math.random() - 0.5).slice(0,SIZE);
-	return;
-}
 
-// console.log(`${MassMass} - ВХОЖДЕНИЕ`)
-// next (MassMass)
-// console.log(MassMass)
-// next (MassMass)
-// console.log(`${MassMass} - ВПЕРЁД`)
-// next(MassMass)
-// console.log(`${MassMass} - ВПЕРЁД`)
-// prev(MassMass)
-// console.log(`${MassMass} - НАЗАД`)
-// prev(MassMass)
-// console.log(`${MassMass} - НАЗАД`)
-// next (MassMass)
-// console.log(`${MassMass} - ВПЕРЁД`)
-// next(MassMass)
-// console.log(`${MassMass} - ВПЕРЁД`)
+word.addEventListener('click', hendleClikc )
+
+
+
+
+const codeKeys = {}
+
+document.addEventListener('keydown', (evt) => {
+	let code = evt.code;
+	let lastWordCode = [...code].slice(-1).join().toLowerCase();
+	let keyRun = evt.key;
+	codeKeys[code] = [lastWordCode, keyRun];
+	console.log(codeKeys)
+})
